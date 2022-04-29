d6d23fbd2498832e790b8b2a000227c25f82e317
DanielusMagnificus <like3venom@gmail.com>
2022-04-29 17:21:23 +0200

    basic text changer

---
+++ test.js
@@ -0,0 +1,7 @@
+
+btnChangeText = document.getElementById("btnChangeText");
+
+btnChangeText.addEventListener("click", function(e) {
+    something = document.getElementById("something");
+    something.innerHTML = "NEW STUFF"
+});
